2f55a902a2ae0e29c12e42a191d885c4a6724e3a
Farshad <f.bagdeli13@gmail.com>
2024-07-08 19:21:18 +0200

    user registration Done

--- src/routers/registrations.js
+++ src/routers/registrations.js
@@ -7,13 +7,32 @@ const prisma = require('../utils/prisma.js')
 
 router.post('/', async (req, res) => {
     // Get the username and password from request body
-    
-    // Hash the password: https://github.com/kelektiv/node.bcrypt.js#with-promises
-    
-    // Save the user using the prisma user model, setting their password to the hashed version
-    
-    // Respond back to the client with the created users username and id
-    res.status(201).json({ user: undefined })
+    const {username , password } = req.body
+
+    try {
+        const userExist = await prisma.user.findUnique({
+            where : { username }
+        })
+        if(userExist) {
+            return res.status(400).json({error : 'Username already exist'})
+        }
+
+        const hashedPassword = await bcrypt.hash(password,10)
+
+        const newUser = await prisma.user.create({
+            data : {
+                username,
+                password : hashedPassword
+            }
+        })
+
+        return res.status(201).json({user : {
+            id: newUser.id, 
+            username : newUser.username
+        }})
+    } catch (error) {
+        res.status(500).json({ error : 'Failed to create User'})
+    }
 });
 
 module.exports = router;
